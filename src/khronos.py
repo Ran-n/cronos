8ad078e6ddfef546a36f8c64af013425f4100efd
ran <ran-n@tutanota.com>
2022-01-15 23:09:53 +0100

    + separar
    + ficheiro excepcions
    + ficheiro de uteis

--- src/khronos.py
+++ src/khronos.py
@@ -1,16 +1,22 @@
 #! /usr/bin/python3
-#------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
 #+ Autor:	Ran#
 #+ Creado:	2019/07/20 12:38:02
-#+ Editado:	2022/01/15 22:23:19.026799
-#------------------------------------------------------------------------------------------------
+#+ Editado:	2022/01/15 23:09:05.792703
+# ------------------------------------------------------------------------------
+
 from datetime import datetime as dt
-#------------------------------------------------------------------------------------------------
+
+from src.khronos_uteis import lazy_check_types
+from src.excepcions import ErroTipado
+# ------------------------------------------------------------------------------
 # agora
-def agora() -> int:
+def agora() -> str:
     """
-    formatador de datas en texto estándar
+    Formatador de datas en texto estándar.
+    Devolvese como str porque é máis versátil, pero un simple cast a int sería
+    suficiente para convertilo a int.
 
     Exemplo:
     +20190710095500000000
@@ -26,7 +32,7 @@ def agora() -> int:
     00 		→ segundo 		→ 2bits
     000000 	→ microsegundo 	→ 6bits
 
-    comando para sacar os valores precisos
+    Comando para sacar os valores precisos
     echo -n '+';date +"%Y-%m-%d %H:%M:%S:%6N"
 
     @entradas:
@@ -41,8 +47,8 @@ def agora() -> int:
 
     valores = [
             agora.year,
-            agora.month,
-            agora.day,
+            '%02d' % agora.month,
+            '%02d' % agora.day,
             agora.hour,
             agora.minute,
             agora.second,
@@ -50,5 +56,39 @@ def agora() -> int:
             ]
 
     return '+'+''.join([str(ele) for ele in valores])
-#------------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------------
+
+# separar
+def separar(agora: str) -> str:
+    """
+    Dado un timestamp tipo khronos devolveo de forma máis dixestible.
+
+    @entradas:
+        agora   -   Requirido   -   Catex
+        └ Co timestamp tipo khronos.
+
+    @saídas:
+        Catex   -   Sempre
+        └ Co timestamp tipo khronos separado para mellor visibilidade.
+    """
+
+    if not lazy_check_types(agora, str):
+        raise ErroTipado('O tipo da variable non é o esperado (catex)')
+
+    if len(agora) < 16:
+        raise ErroTipado('A lonxitude non é a adecuada.')
+
+    if not agora[1:].isdigit():
+        raise ErroTipado('O elemento non é un catex que contén un enteiro')
+
+    return ' '.join([
+                agora[0],           # ac/dc
+                agora[:-16][1:],    # ano
+                agora[-16:-14],     # mes
+                agora[-14:-12],     # día
+                agora[-12:-10],     # hora
+                agora[-10:-8],      # minuto
+                agora[-8:-6],       # segundo
+                agora[-6:]          # microsegundo
+                ])
+
+# ------------------------------------------------------------------------------
